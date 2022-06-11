270d082009910c334bacbb713ac8451168d9845e
nandovbr <nandovbr15@hotmail.com>
2022-06-11 15:32:13 -0300

    ajusta desafio 2

--- desafio2.sql
+++ desafio2.sql
@@ -1,3 +1,3 @@
-SELECT (SELECT COUNT(*) FROM songs) AS cancoes,
-(SELECT COUNT(*) FROM artist) AS artistas,
-(SELECT COUNT(*) FROM albums) AS albuns;
+SELECT (SELECT COUNT(*) FROM SpotifyClone.songs) AS cancoes,
+(SELECT COUNT(*) FROM SpotifyClone.artist) AS artistas,
+(SELECT COUNT(*) FROM SpotifyClone.albums) AS albuns;
