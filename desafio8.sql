5dd4af3cc57984ce697994beae80dbc5be110fe7
nandovbr <nandovbr15@hotmail.com>
2022-06-11 16:42:14 -0300

    adiciona desafio 8

---
+++ desafio8.sql
@@ -0,0 +1,7 @@
+SELECT a.artist_name AS artista,
+alb.album_name AS album
+FROM artist AS a
+JOIN albums AS alb
+ON a.artist_id = alb.artist_id
+WHERE a.artist_name = 'Walter Phoenix'
+ORDER BY alb.album_name;
