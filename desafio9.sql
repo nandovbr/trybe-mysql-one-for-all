38e9a2afa772df0d321834f5733f154b4e16a515
nandovbr <nandovbr15@hotmail.com>
2022-06-11 16:50:53 -0300

    tentando fazer passar o 9 no github.....

--- desafio9.sql
+++ desafio9.sql
@@ -1,4 +1,5 @@
-SELECT COUNT(h.user_id) AS quantidade_musicas_no_historico
+SELECT COUNT(h.user_id)
+AS quantidade_musicas_no_historico
 FROM users AS u
 JOIN historic AS h
 ON u.user_id = h.user_id
